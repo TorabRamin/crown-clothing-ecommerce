5ec034ca367f762ef1ee58e6703037cf5c58da93
Torab Ramin <torabramin@gmail.com>
2019-12-05 01:23:24 +0600

    separate home page into smaller components, finished styling menu item

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
-// import './directory.styles.sass';
+import './directory.styles.scss';
 
 class Directory extends React.Component {
   constructor() {
@@ -47,8 +47,8 @@ class Directory extends React.Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
